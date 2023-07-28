05bac29e6627f0b582e0cb537806658f3195f462
sagaeva <sagaeva@gmail.com>
2023-07-28 11:51:42 +0200

    toegevoegd class User, Role en test-methoden

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/be/shopme/admin/ShopmeBackEndApplication.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/be/shopme/admin/ShopmeBackEndApplication.java
@@ -2,8 +2,10 @@ package be.shopme.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"be.shopme.common.entity", "be.shopme.admin.user"})
 public class ShopmeBackEndApplication {
 
 	public static void main(String[] args) {
